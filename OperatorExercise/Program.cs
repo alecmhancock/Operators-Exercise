6f5145266a5cfb32013cf45ed5cdb9a60462551a
Alec Hancock <alec.m.hancock@gmail.com>
2023-03-06 15:17:53 -0600

    Assignment Complete

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 41;
+            var b = 8;
+
+            var quotient = a / b;
+            var remainder = a % b;
+
+            
+            //Console.WriteLine($"{a} divided by {b} is {quotient} remainder {remainder}");
+            Console.WriteLine(a + " / "  + b + " is " + quotient + " remainder " + remainder);
+
+            Console.WriteLine("Please input a radius below.");
+
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
         }
+        public static double AreaOfCircle(double radius)
+        {
+
+
+
+            //scope of the method below
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+
+        }
+
+
+
+
+
+
+
     }
 }
